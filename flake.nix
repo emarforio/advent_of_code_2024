5f5a76e0554ec74c0548ef68776493318c122198
Erik Marforio <emarforio@gmail.com>
2024-12-02 19:50:22 +0100

    Added flake with kotlin shell

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Development environment for AoC 2024";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            kotlin
+          ];
+        };
+      });
+}
